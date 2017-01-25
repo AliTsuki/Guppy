064e9860bb201fc5bb094031062488aaf5e2f47c
Ali M <PhotonPhighter@gmail.com>
2017-01-24 19:43:00 -0700

    Mostly just rearranged stuff and used print statements to debug

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import re
 #main
 with open("text.txt") as rawdata:
     rawtext = rawdata.read()
+rawdata.close()
 rawtext = rawtext.replace('\n', ' ')
 rawtext = rawtext.replace('\r', ' ')
 rawtext = rawtext.replace('--', ' -- ')
 pat = re.compile(r'([A-Z][^\.!?]*[\.!?])', re.M)
+sentencelist = []
 sentencelist = pat.findall(rawtext)
 firstwordlist = []
 for index, firstword in enumerate(sentencelist):
@@ -17,12 +19,14 @@ for index, firstword in enumerate(sentencelist):
 rawtext = rawtext.replace(', ', ' , ')
 rawtext = rawtext.replace('. ', ' . ')
 rawtext = rawtext.replace('"', ' " ')
-sentencelist = pat.findall(rawtext)
+sentencelistforwords = []
+sentencelistforwords = pat.findall(rawtext)
 wordsinsentencelist = []
 for index, words in enumerate(sentencelist):
 	sentenceindex = int(index)
 	words = sentencelist[sentenceindex].split(' ')
 	wordsinsentencelist.append(words)
+wordlist = []
 wordlist = rawtext.split(' ')
 wordlist = list(filter(None, wordlist))
 wordlistdouble = [[], []]
@@ -33,10 +37,31 @@ for index, word in enumerate(wordlist):
 		wordlistdouble[0].append(wordlist[wordlistindex1])
 		wordlistdouble[1].append(wordlist[wordlistindex2])
 #for wordlistdouble[0]
-rawdata.close()
 
-#firstwordlist = list of words that start sentencelist
+#debugging prints
+print('sentencelist = ')
+print(sentencelist)
+print('\n ---------------------------------  \n')
+print('firstwordlist = ')
+print(firstwordlist)
+print('\n ---------------------------------  \n')
+print('sentencelistforwords = ')
+print(sentencelistforwords)
+print('\n ---------------------------------  \n')
+print('wordsinsentencelist = ')
+print(wordsinsentencelist)
+print('\n ---------------------------------  \n')
+print('wordlist= ')
+print(wordlist)
+print('\n --------------------------------- \n')
+print('wordlistdouble = ')
+print(wordlistdouble)
+print('\n --------------------------------- \n')
+#debugging prints
+
 #sentencelist = list of all sentences
+#firstwordlist = list of words that start sentencelist
+#sentencelistforwords = list of all sentences mutated for ease of extracting words
 #wordsinsentencelist = list of lists containing all of the words in each sentence
 #wordlist = list of all words
 #wordlistdouble = dual list of all words plus the words that follow them
